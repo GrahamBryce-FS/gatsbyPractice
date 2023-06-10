d2db09c4b042a65cbe4bfeacba7b1129d188ea57
Bryce Graham <bgraham1@student.fullsail.edu>
2023-06-10 14:59:54 +0900

    cleaned code

--- src/pages/apiPost.js
+++ src/pages/apiPost.js
@@ -3,6 +3,7 @@ import * as React from "react";
 import Layout from "../components/layout";
 import {
     hero,
+    article
   } from '../components/layout.module.css'
 import react from '../images/react.png'
 
@@ -14,7 +15,9 @@ const ApiPost = () =>{
             <header className={hero}>
                 <img src={react} alt="Main Image" />
             </header>
-            <article>When it comes to best Practices for ReactJS and API's, i think i speak for everyone when i say dotenv is one of (If not the BEST) tool in our arsenal. Hiding my API keys and API endpoints and not being forced to hardcode them into my standard code is super useful! Even though That is more of a lazy man's benefit, It also helps with security as well so double benefit!</article>
+            <article className={article}>When it comes to best Practices for ReactJS and API's, I think I speak for everyone when I say dotenv is one of (If not the BEST) tool in our arsenal. Hiding my API keys and API endpoints and not being forced to hardcode them into my standard code is super useful! Even though that is more of a lazy man's benefit, It also helps with security as well sooo, double benefit!
+            Something else that can be done is that you can use Jest (a testing based software) to see if your API is being handled correctly. Using testing based software will ensure that there is no bugs and that your API operates the way it was intended.
+            </article>
         </Layout>
     )
 };
--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,17 +1,18 @@
 import * as React from "react";
 import Layout from "../components/layout";
 import { Link } from 'gatsby';
+import {h2} from "../components/layout.module.css";
 
 const Blog = () => {
     return(
         <Layout pageTitle="My Blog Posts">
-            <h2><Link to="/ai">Artificial Intelligence</Link></h2>
+            <h2 className={h2}><Link to="/ai">Artificial Intelligence</Link></h2>
                 <p>Wednesday, June 12th 2022</p>
-            <h2><Link to="/cybersecurity">Cyber Security</Link></h2>
+            <h2 className={h2}><Link to="/cybersecurity">Cyber Security</Link></h2>
                 <p>Wednesday, August 14th 2021</p>
-            <h2><Link to="/vr">VR</Link></h2>
+            <h2 className={h2}><Link to="/vr">VR</Link></h2>
                 <p>Saturday, June 15th 2022</p>
-            <h2><Link to="/apiPost">Best Practices for using API's within React</Link></h2>
+            <h2 className={h2}><Link to="/apiPost">Best Practices for using API's within React</Link></h2>
                 <p>Friday, June 9th 2023</p>
         </Layout>
     )
